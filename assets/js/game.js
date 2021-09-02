6011cd1786cec0d6c8b537ae4a72b50b92933591
christi marchetti <christimarchetti@gmail.com>
2021-09-02 15:23:38 -0400

    enemy combat

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,22 @@
 var playerName = window.prompt("What is your robots name?");
-
 var playerHealth = 100;
-
 var playerAttack = 10;
-
 var playermoney = 10;
 
 // You can also log multiple values at once like this console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roberto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+var fight = function(enemyName) {
+    // fight function statements
+     }
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
 
 var fight = function () {
     // Alert players that they are starting the round
@@ -62,4 +68,6 @@ var fight = function () {
     }
 
 }
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
